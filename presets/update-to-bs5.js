8ad410668361d6581c60f79cd53fb49e07945f45
kraken <kraken@templatemonster.me>
2021-06-29 17:16:13 +0300

    SYS: bugfixes

--- presets/update-to-bs5.js
+++ presets/update-to-bs5.js
@@ -17,44 +17,44 @@ module.exports = function (data) {
 				callback(content) {
 					return content
 					// Text
-					.replace(/text(.{0,4})-left/g, 'text$1-start')
-					.replace(/text(.{0,4})-right/g, 'text$1-end')
+					.replace(/\.text(.{0,4})-left/g, '.text$1-start')
+					.replace(/\.text(.{0,4})-right/g, '.text$1-end')
 
 					// Float
-					.replace(/float(.{0,4})-left/g, 'float$1-start')
-					.replace(/float(.{0,4})-right/g, 'float$1-end')
+					.replace(/\.float(.{0,4})-left/g, '.float$1-start')
+					.replace(/\.float(.{0,4})-right/g, '.float$1-end')
 
 					// Border
-					.replace(/border-left/g, 'border-start')
-					.replace(/border-right/g, 'border-end')
+					.replace(/\.border-left/g, '.border-start')
+					.replace(/\.border-right/g, '.border-end')
 
 					// Border-radius
-					.replace(/rounded-left/g, 'rounded-start')
-					.replace(/rounded-right/g, 'rounded-end')
+					.replace(/\.rounded-left/g, '.rounded-start')
+					.replace(/\.rounded-right/g, '.rounded-end')
 
 					// Spacing
-					.replace(/ml-(.)/g, 'ms-$1')
-					.replace(/mr-(.)/g, 'me-$1')
-					.replace(/pl-(.)/g, 'ps-$1')
-					.replace(/pr-(.)/g, 'pe-$1')
+					.replace(/\.ml-(.)/g, '.ms-$1')
+					.replace(/\.mr-(.)/g, '.me-$1')
+					.replace(/\.pl-(.)/g, '.ps-$1')
+					.replace(/\.pr-(.)/g, '.pe-$1')
 
 					// Text
-					.replace(/font-weight-(.)/g, 'fw-$1')
-					.replace(/font-style-(.)/g, 'fst-$1')
+					.replace(/\.font-weight-(.)/g, '.fw-$1')
+					.replace(/\.font-style-(.)/g, '.fst-$1')
 
 					// Gutters
-					.replace(/no-gutters/g, 'g-0')
+					.replace(/\.no-gutters/g, '.g-0')
 
 					// Ratios
-					.replace(/embed-responsive-(\d+)by(\d+)/g, 'ratio-$1x$2')
-					.replace(/embed-responsive/g, 'ratio')
+					.replace(/\.embed-responsive-(\d+)by(\d+)/g, '.ratio-$1x$2')
+					.replace(/\.embed-responsive/g, '.ratio')
 
 					// Modal & Tooltip & Popover
-					.replace(/data-toggle/g, 'data-bs-toggle')
-					.replace(/data-target/g, 'data-bs-target')
-					.replace(/data-dismiss/g, 'data-bs-dismiss')
-					.replace(/data-placement/g, 'data-bs-placement')
-					.replace(/data-original-title/g, 'data-bs-original-title')
+					.replace(/\.data-toggle/g, '.data-bs-toggle')
+					.replace(/\.data-target/g, '.data-bs-target')
+					.replace(/\.data-dismiss/g, '.data-bs-dismiss')
+					.replace(/\.data-placement/g, '.data-bs-placement')
+					.replace(/\.data-original-title/g, '.data-bs-original-title')
 				}
 			}),
 
@@ -65,7 +65,7 @@ module.exports = function (data) {
 				callback(content) {
 					return content
 					// Gutters
-					.replace(/no-gutters/g, 'g-0')
+					.replace(/\.no-gutters/g, '.g-0')
 
 					// Text
 					.replace(/\.font-weight-(.*)/g, '.fw-$1')
@@ -76,20 +76,20 @@ module.exports = function (data) {
 					.replace(/\.embed-responsive/g, '.ratio')
 
 					// Screen reader
-					.replace(/sr-only/g, 'visually-hidden')
+					.replace(/\.sr-only/g, '.visually-hidden')
 
 					// Breakpoints
-					.replace(/media-breakpoint-down\(xl\)/g, 'media-breakpoint-down(xxl)')
-					.replace(/media-breakpoint-down\(lg\)/g, 'media-breakpoint-down(xl)')
-					.replace(/media-breakpoint-down\(md\)/g, 'media-breakpoint-down(lg)')
-					.replace(/media-breakpoint-down\(sm\)/g, 'media-breakpoint-down(md)')
-					.replace(/media-breakpoint-down\(xs\)/g, 'media-breakpoint-down(sm)')
+					.replace(/\.media-breakpoint-down\(xl\)/g, '.media-breakpoint-down(xxl)')
+					.replace(/\.media-breakpoint-down\(lg\)/g, '.media-breakpoint-down(xl)')
+					.replace(/\.media-breakpoint-down\(md\)/g, '.media-breakpoint-down(lg)')
+					.replace(/\.media-breakpoint-down\(sm\)/g, '.media-breakpoint-down(md)')
+					.replace(/\.media-breakpoint-down\(xs\)/g, '.media-breakpoint-down(sm)')
 
-					.replace(/media-breakpoint-between\(xl, xl\)/g, 'media-breakpoint-between(xl, xxl)')
-					.replace(/media-breakpoint-between\(lg, lg\)/g, 'media-breakpoint-between(lg, xl)')
-					.replace(/media-breakpoint-between\(md, md\)/g, 'media-breakpoint-between(md, lg)')
-					.replace(/media-breakpoint-between\(sm, sm\)/g, 'media-breakpoint-between(sm, md)')
-					.replace(/media-breakpoint-between\(xs, xs\)/g, 'media-breakpoint-between(xs, sm)')
+					.replace(/\.media-breakpoint-between\(xl, xl\)/g, '.media-breakpoint-between(xl, xxl)')
+					.replace(/\.media-breakpoint-between\(lg, lg\)/g, '.media-breakpoint-between(lg, xl)')
+					.replace(/\.media-breakpoint-between\(md, md\)/g, '.media-breakpoint-between(md, lg)')
+					.replace(/\.media-breakpoint-between\(sm, sm\)/g, '.media-breakpoint-between(sm, md)')
+					.replace(/\.media-breakpoint-between\(xs, xs\)/g, '.media-breakpoint-between(xs, sm)')
 				}
 			}),
 
